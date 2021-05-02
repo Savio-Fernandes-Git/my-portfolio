bb45cb18ea6865f95cdbd095d7203b9362cf3215
Savio Fernandes <saviof219@gmail.com>
2021-05-02 13:04:40 +0300

    Updated About and home

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import me from '../Pictures/me.jfif';
+import { motion } from 'framer-motion';
 
 export default function About() {
 
@@ -7,6 +9,32 @@ export default function About() {
         <h1 data-aos="fade-right" data-aos-duration="2500" className="aboutTitle ml-32 mt-16 pb-2 text-4xl">
             About
         </h1>
+        <div 
+        className="aboutOne grid grid-cols-1 md:grid-cols-2 grid-flow-row m-32 mt-14 gap-12">
+                <div 
+                data-aos="fade-right" data-aos-duration="2000"
+                >
+                    <motion.img 
+                    drag
+                    dragConstraints={{ left:0,top:0,right:0,bottom:0}}
+                    src={me} alt="My Pic" className="rounded-full border-4 border-white"/>
+                </div>
+                <div data-aos="fade-left" data-aos-duration="2000"
+                className="text-lg leading-7" 
+                >
+                
+
+<p>I am Savio Fernandes, a passionate software developer with experience using the latest development technologies and skills in front-end development. Seeking to raise tech effectiveness by working with my maximum potential through coordination and diligence.</p><br/>
+
+<p>I was always fascinated by how people developed websites and how the whole thing produced a good-looking product on the browser.</p><br/>
+
+<p>I began my journey in web development when I decided I had to do an internship for my undergraduate requirements, since it would provide credit hours for my course. I quickly gathered a curriculum and got into HTML, CSS and Vanilla JavaScript. I spent few hours a day learning new topics, going through courses and learning with practical examples. My short-term goal was to study Reactjs because it was a quite popular framework and it has a huge developer community. Many websites are built on Reactjs. It also allows me to get into React Native which would help me in app development later. </p><br/>
+
+<p>Now, my current motive is to study C# and .Net core for a backend technology. </p><br/>
+
+<p>Besides web development, I love listening & producing music, playing football, basketball and table tennis. </p><br/>
+                </div>
+            </div>
         <div 
         className="about grid grid-cols-1 md:grid-cols-2 grid-flow-row m-32 mt-14 gap-12">
                 <div 
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import "aos/dist/aos.css";
 import me from '../Pictures/me.jfif';
 import { motion } from 'framer-motion';
 import { DiHtml5, DiCss3, DiJsBadge, DiGithubBadge, DiReact, DiVisualstudio } from "react-icons/di";
-import { SiTailwindcss } from "react-icons/si";
+import { SiTailwindcss, SiCsharp } from "react-icons/si";
 import { BiMailSend }  from "react-icons/bi";
 import { FaLinkedin }  from "react-icons/fa";
 
@@ -55,36 +55,41 @@ export default function Home() {
                     src={me} alt="My Pic" className="rounded-full border-4 border-white"/>
                 </span>
             </div>
+
             <h1 data-aos="fade-right" data-aos-duration="2000" className="skills-text text-4xl ml-16">Skillset/Tools used</h1>
             <div className="social-icons" data-aos="fade-up" data-aos-duration="2000" >
                 <motion.div className="icons" variants={buttonVariants} whileHover="hover"
                 data-aos="fade-up" data-aos-duration="2000">
                     <DiHtml5 style={{ height:100, width:100 }}/>
-                    <p className="ml-6 mt-4">HTML 5</p>
+                    <p className="text-center mt-4">HTML 5</p>
                 </motion.div>
                 <motion.div variants={buttonVariants} whileHover="hover" data-aos="fade-up" data-aos-duration="2000">
                     <DiCss3 style={{ height:100, width:100 }}/>
-                    <p className="ml-8 mt-4">CSS 3</p>
+                    <p className="text-center mt-4">CSS 3</p>
                 </motion.div>
                 <motion.div variants={buttonVariants} whileHover="hover" data-aos="fade-up" data-aos-duration="2000">
                     <DiJsBadge style={{ height:100, width:100 }}/>
-                    <p className="ml-2 mt-4">JavaScript</p>
+                    <p className="text-center mt-4">JavaScript</p>
                 </motion.div>
                 <motion.div variants={buttonVariants} whileHover="hover" data-aos="fade-up" data-aos-duration="2000">
                     <DiGithubBadge style={{ height:100, width:100 }}/>
-                    <p className="ml-6 mt-4">Github</p>
+                    <p className="text-center mt-4">Github</p>
                 </motion.div>
                 <motion.div variants={buttonVariants} whileHover="hover" data-aos="fade-up" data-aos-duration="2000">
                     <DiReact style={{ height:100, width:100 }}/>
-                    <p className="ml-6 mt-4">Reactjs</p>
+                    <p className="text-center mt-4">Reactjs</p>
+                </motion.div>
+                <motion.div variants={buttonVariants} whileHover="hover" data-aos="fade-up" data-aos-duration="2000">
+                    <SiCsharp style={{ height:100, width:100 }}/>
+                    <p className="text-center mt-4">C#</p>
                 </motion.div>
                 <motion.div variants={buttonVariants} whileHover="hover" data-aos="fade-up" data-aos-duration="2000">
                     <DiVisualstudio style={{ height:100, width:100 }}/>
-                    <p className="ml-6 mt-4">VScode</p>
+                    <p className="text-center mt-4">VScode</p>
                 </motion.div>
                 <motion.div variants={buttonVariants} whileHover="hover" data-aos="fade-up" data-aos-duration="2000">
                     <SiTailwindcss style={{ height:100, width:100 }}/>
-                    <p className="ml-0 mt-4">TailwindCSS</p>
+                    <p className="text-center mt-4">TailwindCSS</p>
                 </motion.div>
             </div>
             
@@ -93,19 +98,19 @@ export default function Home() {
             <motion.div variants={buttonVariants} whileHover="hover" data-aos="fade-up" data-aos-duration="2000">
                 <a href="https://www.linkedin.com/in/savio15/" target="_blank" rel='noreferrer'>
                     <FaLinkedin style={{ height:100, width:100 }}/>
-                    <p className="ml-4 mt-4">LinkedIn</p>
+                    <p className="text-center mt-4">LinkedIn</p>
                 </a>
             </motion.div>
             <motion.div variants={buttonVariants} whileHover="hover" data-aos="fade-up" data-aos-duration="2000">
                 <a href="https://github.com/Savio-Fernandes-Git" target="_blank" rel='noreferrer'>
                     <DiGithubBadge style={{ height:100, width:100 }}/>
-                    <p className="ml-6 mt-4">GitHub</p>
+                    <p className="text-center mt-4">GitHub</p>
                 </a>
             </motion.div>
             <motion.div variants={buttonVariants} whileHover="hover" data-aos="fade-up" data-aos-duration="2000">
                 <a href="mailto:savio-f-tech@gmail.com" target="_blank" rel='noreferrer'>
                     <BiMailSend style={{ height:100, width:100 }}/>
-                    <p className="ml-10 mt-4">Mail</p>
+                    <p className="text-center ml-2 mt-4">Mail</p>
                 </a>
             </motion.div>
             </div>
